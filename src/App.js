616c82347366e1283872ee67c8381a7c6810bb25
Mauricio Hidalgo Rios <mauriciohidalgorios@Mauricios-MacBook-Pro-2.local>
2023-02-05 18:44:40 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import React from "react";
 import logo from './logo.svg';
+import TodoItem from "./Components/TodoItem";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(){
+    super() 
+    this.state = {
+      todos: [
+        {id: 1, text: 'Sacar la basura'},
+        {id: 2, text: 'Pasear el perro'},
+        {id: 3, text: 'Cerrar las puertas'},
+      ],
+      newTodo: '',
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>TODO</h1>
+          <ul>
+            {this.state.todos.map((todo) => {
+              return (
+                <TodoItem key={todo.id} text={todo.text}/>
+              )
+            })}
+          </ul>
+          <img src={logo} className="App-logo" alt="logo" />
+          <form onSubmit={this.handleSubmit}>
+            <input 
+              type="text"
+              value={this.state.newTodo} 
+              onChange={this.handleChange}
+            />
+            <button type="submit">Add TODO</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
+  
+  handleChange = (event) => {
+    this.setState({newTodo: event.target.value})
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.setState( prevState => ({
+      todos: [...prevState.todos, {id: Date.now(), text: prevState.newTodo}],
+      newTodo: '',
+    }))
+  }
+
 }
 
 export default App;
